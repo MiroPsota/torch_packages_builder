d382177f7c96ec15d941e8e8be434c53cf0dcfd4
Miroslav Psota <9453736+MiroPsota@users.noreply.github.com>
2024-07-21 16:23:44 +0200

    Add fairseq specific changes

--- package_specific.sh
+++ package_specific.sh
@@ -9,7 +9,7 @@ if [[ $REPO == "facebookresearch/Mask2Former" ]] \
   patch -p0 < "$SCRIPT_DIR"/package_specific/Mask2Former_cpu.patch
 fi
 
-if [[ $REPO == "rusty1s/pytorch_cluster" ]] \
+if { [[ $REPO == "rusty1s/pytorch_cluster" ]] || [[ $REPO == "facebookresearch/fairseq" ]]; } \
   && [[ $OS == "Windows" ]] \
   && [[ ${TORCH_VERSION:0:4} == "1.12" ]] \
   && [[ $COMPUTE_PLATFORM == "cu116" ]]; then
@@ -19,9 +19,10 @@ if [[ $REPO == "rusty1s/pytorch_cluster" ]] \
   patch -d "$TORCH_PYBIND_DIR" < "$SCRIPT_DIR"/package_specific/torch_pybind_cast_h.patch
 fi
 
-if [[ $REPO == "facebookresearch/pytorch3d" ]]; then
+if [[ $REPO == "facebookresearch/pytorch3d" ]] || [[ $REPO == "facebookresearch/fairseq" ]]; then
   CUB_VERSION=""
   if [[ $OS == "Windows" ]] \
+    && [[ $REPO == "facebookresearch/pytorch3d" ]] \
     && { [[ $COMPUTE_PLATFORM == "cu117" ]] || [[ $COMPUTE_PLATFORM == "cu118" ]] || [[ $COMPUTE_PLATFORM == "cu121" ]]; }; then
     CUB_VERSION="1.17.2"
   fi
@@ -35,8 +36,15 @@ if [[ $REPO == "facebookresearch/pytorch3d" ]]; then
     curl -L https://github.com/NVIDIA/cub/archive/${CUB_VERSION}.tar.gz | tar -xzf - --strip-components=1 --directory cub
     echo "CUB_HOME=$PWD/cub" >> "$GITHUB_ENV"
   fi
+fi
 
-  if [[ $OS == "Linux" ]] && [[ $COMPUTE_PLATFORM == "cu102" ]]; then
-    patch -p0 < "$SCRIPT_DIR"/package_specific/pytorch3d_cpp14.patch
-  fi
+if [[ $REPO == "facebookresearch/pytorch3d" ]] \
+  && [[ $OS == "Linux" ]] \
+  && [[ $COMPUTE_PLATFORM == "cu102" ]]; then
+  patch -p0 < "$SCRIPT_DIR"/package_specific/pytorch3d_cpp14.patch
+fi
+
+if [[ $REPO == "facebookresearch/fairseq" ]]; then
+  pip install cython
+  patch -p0 < "$SCRIPT_DIR"/package_specific/fairseq_cub.patch
 fi
