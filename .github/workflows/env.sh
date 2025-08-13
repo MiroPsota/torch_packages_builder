8a955b85474b7502af1ffdf4d1b7507465c5e668
Miroslav Psota <9453736+MiroPsota@users.noreply.github.com>
2025-08-13 20:37:32 +0200

    Change MAX_JOBS logic

--- .github/workflows/env.sh
+++ .github/workflows/env.sh
@@ -1,23 +1,12 @@
 #! /bin/bash
 
-case $OS in
-  "Linux")
-    export MAX_JOBS=$(nproc)
-    ;;
-  "macOS")
+export MAX_JOBS=${MAX_JOBS:-4}
+
+if [[ $OS == "macOS" ]]; then
     export CC=clang
     export CXX=clang++
     export MACOSX_DEPLOYMENT_TARGET=11.0
-    export MAX_JOBS=$(sysctl -n hw.ncpu)
     if [[ $(arch) == "macos-14" ]]; then
       export ARCHFLAGS="-arch arm64"
     fi
-    ;;
-  "Windows")
-    export MAX_JOBS=$(echo %NUMBER_OF_PROCESSORS%)
-    ;;
-  *)
-    echo "OS not known"
-    exit 1
-    ;;
-esac
+fi
